b4223b06b9e5c4d749ce5ba34ff421ca4c8e0830
Pedro de Souza Martins <143435476+SinPedro@users.noreply.github.com>
2023-09-18 15:32:08 -0300

    Add files via upload

---
+++ desafioArrays/desafio18.js
@@ -0,0 +1,10 @@
+const ArraysFrutas = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"]
+const BuscaArrays = ArraysFrutas["2"];
+
+
+
+
+
+console.log("ABAIXO ESTÃO AS INFORMAÇÕES DO ARRAY DE ÍNDICE 3 E EMBAIXO SEU TAMANHO:");
+console.log(BuscaArrays);
+console.log(ArraysFrutas.length)
